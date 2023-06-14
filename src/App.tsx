a3c617c0ffcfa0d0edebf2707e27b6188ad414de
bloodredhood <3drusher@gmail.com>
2023-06-15 00:39:00 +0200

    [feat] add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css'
+import Analysis from './components/Analysis/Analysis'
+import Header from './components/Header/Header'
+import Main from './components/Main/Main'
+import { Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
     <>
       <Header />
-      
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/analysis" element={<Analysis />} />
+      </Routes>
     </>
   )
 }
---
+++ src/components/Analysis/Analysis.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+
+const Analysis: FC = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default Analysis
